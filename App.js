993fce72fd71366b179c2762245bab691f8c3819
harika6672 <rajasiri1998@gmail.com>
2025-06-21 02:43:18 +0530

    installed react, react dom and parcel to app

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Hello world from React!"
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(heading);
